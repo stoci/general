1c6ba01d65e457e026315ccf718806baa5e82486
stoci <jablitz@ncsu.edu>
2014-06-01 15:13:29 -0400

    added FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+def FizzBuzz():
+	for x in range(1,101):
+		if(x%3==0 and x%5==0): 
+			print("FizzBuzz") 
+			continue
+		elif(x%3==0): 
+			print("Fizz")
+			continue
+		elif(x%5==0): 
+			print("Buzz")
+			continue
+		print(x)
+
+FizzBuzz()
